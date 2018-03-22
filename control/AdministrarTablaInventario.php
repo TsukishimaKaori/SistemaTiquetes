bc3c6153dbe5ecdf14f748f8fe79498b84c76ae9
HP_810G2 <HP_810G2@DESKTOP-HFVR94I>
2018-03-21 20:54:40 -0600

    Nuevo archivo .php AdministracionInventario y agregarDispositivo ademas inventario.css

---
+++ control/AdministrarTablaInventario.php
@@ -0,0 +1,10 @@
+<?php
+
+
+function cabeceraTabla(){
+    echo "<th>id</th>"
+    . "<th>nombre</th>"
+            . "<th>tipo</th>"
+            . "<th>bien</th>";
+    
+}
--- vista/AdministrarInventario.php
+++ vista/AdministrarInventario.php
@@ -2,15 +2,16 @@
 <html> 
     <head>
         <meta charset="UTF-8">
-        <?php require_once ("../control/ArchivosDeCabecera.php");   ?>
+        <?php require_once ("../control/ArchivosDeCabecera.php"); ?>
         <link rel="stylesheet" href="../recursos/bootstrap/css/bootstrap-datetimepicker.min.css" />
         <script src="../recursos/bootstrap/js/bootstrap-datetimepicker.min.js"></script>
         <script src="../recursos/bootstrap/js/es.js"></script>   
         <link href="../recursos/css/inventario.css" rel="stylesheet"/> 
+        <?php require ("../control/AdministrarTablaInventario.php"); ?>
     </head>
     <body>           
         <?php require ("../vista/Cabecera.php"); ?>   
-        
+
         <div class="container-fluid">
             <div class="row">
                 <div class=""></div>
@@ -21,26 +22,75 @@
                         <button class="tablinks" onclick="">Licencias</button>
                         <button class="tablinks" onclick="">Repuestos</button>
                     </div>                                   
-                    <section id="admin-empresas" class="tabcontent">
-                        <h1>Inventario</h1>
+                    <section id="tab-activos" class="tabcontent">
+                        <h1>Activos</h1>
                         <div class="container-fluid">
                             <div class="row">
-                               
+                                <div class="col-md-2"></div>                                
+                                <div id ="" class="col-md-8"></div>                
+                                <div class="col-md-2">                    
+                                    <button type="button" class="btn btn-success  btn-circle btn-xl" data-toggle="modal" data-target=""><i class="glyphicon glyphicon-plus"></i></button>         
+                                </div>
                             </div>
                             <div class="row">  
-                                <div class=""></div>
-                               
+                                <div class="col-md-1"></div>
+                                <div class="col-md-10 ">
+                                    <div class="table table-responsive">  
+                                        <table class="table table-hover">
+                                            <thead>
+                                                <?php cabeceraTabla();?>                                 
+                                            </thead>
+                                            <tbody>
+                                                <?php ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </section>
-                   
+                    <section id="tab-pasivos" class="tabcontent tab-oculto">
+                        <h1>Pasivos</h1>
+                        <div class="container-fluid">
+                            <div class="row">                               
+                            </div>
+                            <div class="row">  
+                                <div class=""></div>                               
+                            </div>
+                        </div>
+                    </section>
+
+                    <section id="tab-licencias" class="tabcontent tab-oculto">
+                        <h1>Licencias</h1>
+                        <div class="container-fluid">
+                            <div class="row">                               
+                            </div>
+                            <div class="row">  
+                                <div class=""></div>                               
+                            </div>
+                        </div>
+                    </section>
+
+                    <section id="tab-repuestos" class="tabcontent tab-oculto">
+                        <h1>Repuestos</h1>
+                        <div class="container-fluid">
+                            <div class="row">                               
+                            </div>
+                            <div class="row">  
+                                <div class=""></div>                               
+                            </div>
+                        </div>
+                    </section>
+
+
+
                 </div>
                 <div class="col-md-1"></div>
             </div>
         </div>
-        
-        
-        
-        
+
+
+
+
     </body>
 </html>
