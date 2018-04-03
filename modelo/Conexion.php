e616b30052856706b20c302af63e8c8636cb82fe
HP_810G2 <HP_810G2@DESKTOP-HFVR94I>
2018-04-03 14:18:35 -0600

    fran

--- modelo/Conexion.php
+++ modelo/Conexion.php
@@ -25,8 +25,8 @@ class Conexion {
         return $this->conn;
     }
 
-     private $serverName = "DANNY\SQLEXPRESS01";
-    //private $serverName = "DESKTOP-HFVR94I\SQLEXPRESS01";     
+    // private $serverName = "DANNY\SQLEXPRESS01";
+    private $serverName = "DESKTOP-HFVR94I\SQLEXPRESS01";     
    // private $serverName = "TATIANA\SQLEXPRESS02";
 
     //Conexion para el hosting gratuito
