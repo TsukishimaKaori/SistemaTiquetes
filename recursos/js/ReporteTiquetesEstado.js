f3093581d58ce8b2b0594ac48c1f8214522aa13f
Danny <Danny@DANNY>
2018-05-20 15:26:33 -0600

    Merge origin/master
    Conflicts:
    	modelo/Conexion.php
    	modelo/Service.php

--- recursos/js/ReporteTiquetesEstado.js
+++ recursos/js/ReporteTiquetesEstado.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
     tablaTiquetes();
-
+    graficoAreas();
 });
 
 function tablaTiquetes() {
-    $('#tablaTiquetes').DataTable({
+    $('#mitablaTiquetes').DataTable({
         "language": {
             "sProcessing": "Procesando...",
             "sLengthMenu": "Mostrar _MENU_ registros",
@@ -36,31 +36,30 @@ function tablaTiquetes() {
 }
 
 
-function FiltrarTiquete() {
-    var estado = $("#estado").val();
+function FiltrarTiquete(estado) {
+
 
     $.ajax({
         data: {'estado': estado
-        },
-        dataType: 'json',
+        },        
         type: 'POST',
         url: '../control/SolicitudAjaxReporteTiquetesEstado.php',
         beforeSend: function () {
             $("#cargandoImagen").css('display', 'block');
         },
         success: function (response) {
-            $("#cargandoImagen").css('display', 'none')
-            $('#CantidadInfo').html(response['cantidad']);
-            $('#tablaTiquetes').DataTable().destroy();
-            $("#tbodyTiquetes").html(response['tiquetes']);
+            $("#cargandoImagen").css('display', 'none')            
+            $('#mitablaTiquetes').DataTable().destroy();
+            $("#TablaTiquetes").html(response);
             tablaTiquetes();
-
+           
         }
 
     });
 }
 function mostrarDetalleTIquetes($codigo) {
 
+    $("#modalInfoTiquetes").modal("hide");
     $.ajax({
         data: {'codigo': $codigo
         },
@@ -72,9 +71,84 @@ function mostrarDetalleTIquetes($codigo) {
         success: function (response) {
             $("#cargandoImagen").css('display', 'none')
             $('#infoTiquete').html(response);
-
-
+            $("#modalInfoTiquetes").modal("show");
+           
         }
 
+    });
+}
+
+function graficoAreas() {
+    $("#chart-area").empty();
+    $('#tbodySolicitudClasificacionPorArea').empty();
+    $.ajax({
+        data: {'graficoPieTiquetesEstado': true},
+        type: 'POST',
+        url: '../control/SolicitudAjaxReporteTiquetesEstado.php',
+        beforeSend: function () {
+            $("#cargandoImagen").css('display', 'block');
+        },
+        success: function (response) {
+            $("#cargandoImagen").css('display', 'none')
+            var vector = JSON.parse(response);
+            var cantidad = [];
+            var descripcion = [];
+            var colores = [];
+            for (var i in vector) {
+                cantidad.push(vector[i]['cantidad']);
+                descripcion.push(vector[i]['descripcion']);
+                r = Math.floor(Math.random() * 200);
+                g = Math.floor(Math.random() * 200);
+                b = Math.floor(Math.random() * 200);
+                color = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+                colores.push(color);
+                var fila = '<tr onclick="FiltrarTiquete(\'' + vector[i]['descripcion'] + '\')"><td>' + vector[i]['descripcion'] + '</td><td>' + vector[i]['cantidad'] + '</td></tr>';
+                $('#tbodySolicitudClasificacionPorEstado').append(fila);
+
+            }
+            if (typeof myPie == 'undefined') {
+                config = {
+                    type: 'pie',
+                    data: {
+                        datasets: [{
+                                data: cantidad,
+                                backgroundColor: colores,
+                                label: 'Clasificaciones'
+                            }],
+                        labels: descripcion
+                    },
+                    options: {
+                        responsive: true
+                    }
+                };
+                var ctx = document.getElementById('chart-area').getContext('2d');
+                var canvas = document.getElementById("chart-area");
+                var myNewChart=window.myPie = new Chart(ctx, config);
+                canvas.onclick = function (evt) {
+                    var activePoints = myNewChart.getElementsAtEvent(evt);
+                    if (activePoints[0]) {
+                        var chartData = activePoints[0]['_chart'].config.data;
+                        var idx = activePoints[0]['_index'];
+
+                        var label = chartData.labels[idx];                        
+                        FiltrarTiquete(label);
+                    }
+                };
+            } else {
+                config.data.datasets.splice(0, 1);
+                var tamanio = config.data.labels.length;
+                config.data.labels.splice(0, tamanio);
+                var newDataset = {
+                    data: cantidad,
+                    backgroundColor: colores,
+                    label: 'Clasificaciones'
+                };
+                var des = descripcion;
+                config.data.datasets.push(newDataset);
+                config.data.labels = descripcion;
+                window.myPie.update();
+            }
+
+        }
     });
 }
